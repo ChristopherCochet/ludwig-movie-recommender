f574b95c1af3997ec4e66741c8d1fc200ee7bcb9
Christopher Cochet <39317551+ChristopherCochet@users.noreply.github.com>
2023-03-26 03:30:06 +0000

    updated rotten tomato example from the ludwig website

--- ludwig_train.py
+++ ludwig_train.py
@@ -1,6 +1,11 @@
 from ludwig.api import LudwigModel
 import pandas
 
-df = pandas.read_csv('rotten_tomatoes.csv')
-model = LudwigModel(config='rotten_tomatoes.yaml')
-results = model.train(dataset=df)
+# df = pandas.read_csv('rotten_tomatoes.csv')
+# model = LudwigModel(config='rotten_tomatoes.yaml')
+# results = model.train(dataset=df)
+
+model = LudwigModel.load('results/experiment_run_6/model')
+
+predictions, _ = model.predict(dataset='rotten_tomatoes_test.csv')
+print(predictions.head())
